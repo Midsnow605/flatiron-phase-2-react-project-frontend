7b901ce3b3f9d7b246608376c82bb6b63c2a1d32
Jose Gutierrez <gutierrez.jose24@gmail.com>
2023-10-08 04:45:13 -0500

    updated About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,27 @@ const About = () => {
   return (
     <div>
       <h2>About Us</h2>
-      <p>About</p>
+      <p>
+        Welcome to Anime Lister! We are a platform dedicated to anime enthusiasts, providing a space to discover,
+        share, and connect with fellow fans.
+      </p>
+
+      <h3> Goals </h3>
+      <p>
+        My mission is to create a place where anime lovers can engage with each other, discuss their
+        favorite series, characters, and moments, and stay updated with the latest in the anime world.
+      </p>
+
+      <h3>What We Offer</h3>
+      <ul>
+        <li>Discover a vast collection of anime series and movies.</li>
+        <li>Share your thoughts and reviews on anime you've watched.</li>
+        <li>Connect with like-minded individuals who share your passion for anime.</li>
+        <li>Stay updated with the latest news, releases, and trends in the anime industry.</li>
+      </ul>
+
     </div>
   );
-}
+};
 
 export default About;
