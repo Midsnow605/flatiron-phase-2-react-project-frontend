edcee1ffda4fc95a94671f36a3daa53f06165529
Jose Gutierrez <gutierrez.jose24@gmail.com>
2023-10-07 20:00:45 -0500

    Submit form component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import SubmitForm from './SubmitForm';
 
 const Home = () => {
   const [animeData, setAnimeData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // Fetch anime data
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:3000/anime');
@@ -23,9 +25,32 @@ const Home = () => {
     fetchData();
   }, []);
 
+  const handleFormSubmit = async (formData) => {
+    try {
+      const response = await fetch('http://localhost:3000/anime', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      // Refresh anime data after successful submission
+      const updatedData = await response.json();
+      setAnimeData(updatedData);
+    } catch (error) {
+      console.error('Error submitting data:', error);
+    }
+  };
+
   return (
     <div className="home-container">
       <h2>Welcome to the Home Page!</h2>
+      <SubmitForm onFormSubmit={handleFormSubmit} /> {/* Render the SubmitForm component */}
       {loading ? (
         <p>Loading data...</p>
       ) : (
---
+++ src/pages/SubmitForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const SubmitForm = ({ onFormSubmit }) => {
+  const [formData, setFormData] = useState({ image: '', name: '', description: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onFormSubmit(formData);
+    setFormData({ image: '', name: '', description: '' }); // Reset the form after submission
+  };
+
+  return (
+    <div>
+      <h3>Add New Anime:</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Image URL:
+          <input type="text" name="image" value={formData.image} onChange={handleChange} />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </label>
+        <label>
+          Description:
+          <textarea name="description" value={formData.description} onChange={handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SubmitForm;
